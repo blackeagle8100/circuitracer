982e1dd3111dbba727cc7bdd19c120185f11dfca
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2026-02-16 04:23:26 +0100

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,21 @@
+// supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://itmhwyamdcdcompixnrk.supabase.co';
+const supabaseKey = 'sb_publishable_lun6Mhjl_0P4vqQNaPGyKw_OKNwJ9cW';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export functies zodat main.js ze kan importeren
+export function uploadScore(playerName, bestLap, currentTable) {
+    if (!playerName || bestLap == null) return;
+    supabase.from(currentTable).insert([{ player_name: playerName, lap_time: bestLap }])
+    .then(({ data, error }) => {
+        if (error) console.error('Supabase error:', error);
+        else console.log(`Score uploaded to ${currentTable}:`, data);
+    });
+}
+
+export function uploadEndScores(players, currentTable) {
+    if (players.P1.lap > 0) uploadScore(players.P1.name, players.P1.bestLap, currentTable);
+    if (players.P2.lap > 0) uploadScore(players.P2.name, players.P2.bestLap, currentTable);
+}
